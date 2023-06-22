d013d9879166712d97798243c57ea713856fbd39
epiler <epiler447@gmail.com>
2023-06-21 20:01:31 -0500

    se crean timer

--- app/src/main/java/com/example/puebliando/MainActivity.java
+++ app/src/main/java/com/example/puebliando/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.puebliando;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    // atributos = elementos de la actividad
+
+    MediaPlayer cancion;
+
+    //metodos
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cancion = MediaPlayer.create(this,R.raw.miguel );
+        cancion.start();
+
+        //temporizamos el splash
+
+        TimerTask inicioAPP = new TimerTask() {
+            @Override
+            public void run() {
+                //lanzo el home
+
+                Intent lanzamiento  = new Intent(MainActivity.this, Home.class);
+                startActivity(lanzamiento);
+
+
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(inicioAPP, 10000);
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/puebliando/Sitios.java
@@ -0,0 +1,13 @@
+package com.example.puebliando;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sitios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sitios);
+    }
+}
